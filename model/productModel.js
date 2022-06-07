0bc453ccf9d8c9eaf890df1ad28f125b6eeb0af2
leandro <martinezleandro2004@gmail.com>
2022-06-07 14:04:05 -0300

    added scrollbar styles

--- model/productModel.js
+++ model/productModel.js
@@ -1,11 +1,47 @@
-// En productModel.js deben crear un array con 6 productos. 
-// Cada producto será un objeto que tenga name, description, image, price. 
+// En productModel.js deben crear un array con 6 productos.
+// Cada producto será un objeto que tenga name, description, image, price.
 // Recordá que ambos arrays deben ser accesibles desde otra parte así que utilizar la palabra export
 export const productos = [
-  {name: 'producto1', description: 'Proin condimentum sodales risus ut aliquet. ', image: 'images/producto1.jpg', price: 100000, btn: 'Agregar al carrito'},
-  {name: 'producto2', description: 'Proin condimentum sodales risus ut aliquet. ', image: 'images/producto2.jpg', price: 100000, btn: 'Agregar al carrito'},
-  {name: 'producto3', description: 'Proin condimentum sodales risus ut aliquet. ', image: 'images/producto3.jpg', price: 100000, btn: 'Agregar al carrito'},
-  {name: 'producto4', description: 'Proin condimentum sodales risus ut aliquet. ', image: 'images/producto4.jpg', price: 100000, btn: 'Agregar al carrito'},
-  {name: 'producto5', description: 'Proin condimentum sodales risus ut aliquet. ', image: 'images/producto5.jpg', price: 100000, btn: 'Agregar al carrito'},
-  {name: 'producto6', description: 'Proin condimentum sodales risus ut aliquet. ', image: 'images/producto6.jpg', price: 100000, btn: 'Agregar al carrito'}
-]
+  {
+    name: "Producto 1",
+    description: "Proin condimentum sodales risus ut aliquet. ",
+    image: "images/producto1.jpg",
+    price: 100000,
+    btn: "Agregar al carrito",
+  },
+  {
+    name: "Producto 2",
+    description: "Proin condimentum sodales risus ut aliquet. ",
+    image: "images/producto2.jpg",
+    price: 100000,
+    btn: "Agregar al carrito",
+  },
+  {
+    name: "Producto 3",
+    description: "Proin condimentum sodales risus ut aliquet. ",
+    image: "images/producto3.jpg",
+    price: 100000,
+    btn: "Agregar al carrito",
+  },
+  {
+    name: "Producto 4",
+    description: "Proin condimentum sodales risus ut aliquet. ",
+    image: "images/producto4.jpg",
+    price: 100000,
+    btn: "Agregar al carrito",
+  },
+  {
+    name: "Producto 5",
+    description: "Proin condimentum sodales risus ut aliquet. ",
+    image: "images/producto5.jpg",
+    price: 100000,
+    btn: "Agregar al carrito",
+  },
+  {
+    name: "Producto 6",
+    description: "Proin condimentum sodales risus ut aliquet. ",
+    image: "images/producto6.jpg",
+    price: 100000,
+    btn: "Agregar al carrito",
+  },
+];
